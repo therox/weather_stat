da5a4af65ccba76ccbf1fee0cbeb96cac2e9dc35
Sergey Alipchenkov <stherox@smail.com>
2021-05-20 17:15:46 +0300

    Использование SQL

--- db/weather.py
+++ db/weather.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 
 # Таблица weather, состоящая из следующих колонок:
 # 1) geom        - геометрия
@@ -10,12 +11,12 @@ import psycopg2
 # SRID=4326;POINT (1 2)
 
 # CREATE TABLE weather (
-#     id        serial PRIMARY KEY,
+#     id         serial PRIMARY KEY,
 #     geom       GEOMETRY NOT NULL,
-#     temp         numeric NOT NULL,
+#     temp       numeric NOT NULL,
 #     pressure   numeric NOT NULL,
-#     humidity        numeric NOT NULL,
-#     date         date
+#     humidity   numeric NOT NULL,
+#     date       date
 # );
 
 conn = psycopg2.connect(dbname='weather_stat',
@@ -28,9 +29,18 @@ cursor.execute(
     "INSERT INTO weather (geom, temp, pressure, humidity, date) VALUES ('SRID=4326;POINT (1 2)', 24, 765.3, 46, now())"
 )
 
-cursor.execute('SELECT * FROM weather LIMIT 10')
-records = cursor.fetchall()
-print(records[0])
+with conn.cursor() as cursor:
+    columns = ('id', 'geom', 'pressure', 'humidity', 'date')
+    stmt = sql.SQL('SELECT {} FROM {} LIMIT 5').format(
+        sql.SQL(',').join(map(sql.Identifier, columns)),
+        sql.Identifier('weather'))
+    cursor.execute(stmt)
+    for row in cursor:
+        print(row)
 
-cursor.close()
-conn.close()
+# cursor.execute('SELECT * FROM weather LIMIT 10')
+# records = cursor.fetchall()
+# print(records[0])
+
+# cursor.close()
+# conn.close()
