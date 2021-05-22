eb6c040be89b08feb6191ca7a2312e9c5c48a7a7
Sergey Alipchenkov <stherox@gmail.com>
2021-05-22 21:57:59 +0300

    Докерфайл для основного приложения

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Запускаем постгрес
+su - docker -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
+
 # uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
 uwsgi --ini wsgi.ini
 # http://127.0.0.1:5000/?x=2&y=3&start=1-2-2021&end=1-2-2001
